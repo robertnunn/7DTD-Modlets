466384429a6bcd46b8a72c4e35c7a21fcf1a73a5
Rob <robnunn10@gmail.com>
2020-01-12 23:59:03 -0500

    only zombie spawns v1

--- Python Scripts/scratch.py
+++ Python Scripts/scratch.py
@@ -3,32 +3,24 @@ import os
 from pprint import pprint as pp
 
 path = "D:/Games/7DTD/7DTD-Modlets/Alpha 18/A18-Config/"
-filenames = {'progression.xml': './perk/perks', 'item_modifiers.xml': './item_modifier'}
+filename = 'entitygroups.xml'
+blank_animal = 'invisibleAnimal'
+blank_enemy = 'invisibleAnimalEnemy'
 
-def get_effects(path, filename, find_string):
-    path += filename
-    print(path)
-    tree = et.parse(path)
-    effect_set = set()
+groups_tree = et.parse(path+filename)
+groups = groups_tree.findall('./entitygroup/entity')
+full_entity_set = set()
+zombie_set = set()
+non_zombie_set = set()
 
-    nodes = tree.findall("./perks/perk")
-    for node in nodes:
-        name = node.get('name')
-        print(name)
-        effects = node.findall('./effect_group/passive_effect')
-        for eff in effects:
-            effect_set.add(eff.get('name'))
-    
-    return effect_set
+for i in groups:
+    name = i.get('name')
+    full_entity_set.add(name)
+    if 'zombie' not in name.lower():
+        non_zombie_set.add(name)
+    else:
+        zombie_set.add(name)
 
-master_set = set()
-for i in filenames:
-    temp_set = get_effects(path, i, filenames[i])
-    master_set = master_set.union(temp_set)
-
-print(master_set)
-
-mod_effect_list = '\n'.join(sorted(list(master_set)))
-with open('effect list.txt', mode='w') as csv_file:
-    csv_file.write(mod_effect_list)
-# pp(hp_list)
+# pp(entity_set)
+pp(non_zombie_set)
+pp(zombie_set)
